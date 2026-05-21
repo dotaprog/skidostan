0d5e2a493d6a044caab7da46326d3ca627cb9be3
dotaprog <dotakassir@gmail.com>
2026-05-21 18:49:02 +0700

    Update promocodes.ts

--- src/data/promocodes.ts
+++ src/data/promocodes.ts
@@ -156,10 +156,10 @@ export const promoCodes: PromoCode[] = [
     id: "food-5",
     discount: "36%",
     company: "Магнит Доставка",
-    logo: "/logos/magnit.svg",
+    logo: "/logos/magnit.png",
     description:
       "На первый заказ от 1.200₽ в разделе «Экспресс». Не распространяются на товары из разделов: «М.Косметик», «Магнит Гипермаркет», «Аптека» и «Финальная цена».",
-    code: "TEST",
+    code: "PE3I99LL",
     category: "food",
   },
   {
@@ -302,19 +302,19 @@ export const promoCodes: PromoCode[] = [
     id: "food-21",
     discount: "51%",
     company: "Магнит Доставка",
-    logo: "/logos/magnit.svg",
+    logo: "/logos/magnit.png",
     description:
       "На первый заказ от 1.000 ₽ в разделе Мигом. Скидка по промокоду распространяется только на товары из раздела «Мигом», не распространяется на товары с пометкой «финальная цена».",
-    code: "TEST",
+    code: "FM51PQMKY",
     category: "food",
   },
   {
     id: "food-22",
     discount: "21%",
     company: "Магнит Доставка",
-    logo: "/logos/magnit.svg",
+    logo: "/logos/magnit.png",
     description: "На повторный заказ от 1.700 ₽ в разделе «Мигом» Бонусы суммируются со скидками.",
-    code: "TEST",
+    code: "21DSF164BD9",
     category: "food",
   },
   {
